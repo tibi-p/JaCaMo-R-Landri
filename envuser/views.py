b2fcc457bd1b1f8c3c92d14b3e45331ba13c0422
Tiberiu Popa <popa.tiberiu@gmail.com>
2012-05-12 20:13:04 +0300

    Added neat table renderer.

--- envuser/views.py
+++ envuser/views.py
@@ -4,12 +4,28 @@ from django.template import RequestContext
 from envuser.models import EnvUser
 from solution.models import Solution
 from subenvironment.models import SubEnvironment
+import django_tables2 as tables
+from django_tables2.utils import A
+
+class EnvUserTable(tables.Table):
+    user = tables.LinkColumn('envuser.views.detail', args=[ A('pk') ],
+        order_by=('user.username', '-rank'))
+
+    class Meta:
+        model = EnvUser
+        fields = ('user', 'rank', 'economy')
+        sequence = ('user', 'rank', 'economy')
+        attrs = { 'style': 'text-align: center;' }
+        order_by = ('-rank', 'user')
 
 @login_required
 def index(request):
-    envUserList = EnvUser.objects.all()
+    envUserTable = EnvUserTable(EnvUser.objects.all())
+    tables.RequestConfig(request, paginate={
+        'per_page': 25
+    }).configure(envUserTable)
     return render_to_response('envuser/index.html',
-        { 'envUserList': envUserList },
+        { 'envUserTable': envUserTable },
         context_instance = RequestContext(request))
 
 @login_required
