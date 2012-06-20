6dfe7a81e1e635a53603f66705cfe5e93c91dd70
andreigc <andrei.geacar@gmail.com>
2012-06-20 16:53:42 +0300

    Created an object that for a user's solution specification builds an XML file

---
+++ solution/solution.py
@@ -0,0 +1,44 @@
+from xml.dom.minidom import Document
+
+class Solution():
+    
+    asl_list = [{'agentID':'ag1','cardinality':1, 'file':'ag1.asl'},
+                {'agentID':'ag2','cardinality':2, 'file':'ag2.asl'}]
+    artifacts_jar = 'artifacts.jar'
+    org_zip = 'orgs.zip'
+    
+    #Generate XML file from object
+    def make_xml(self):
+        doc = Document()
+        root = doc.createElement('solution')
+        doc.appendChild(root)
+        
+        node = doc.createElement('asl-list')
+        
+        for dic in self.asl_list:
+            agentNode = doc.createElement('asl')
+            for attr in dic.keys():
+                agentNode.setAttribute(attr,unicode(dic[attr]))
+            node.appendChild(agentNode)
+        root.appendChild(node)
+        
+        node = doc.createElement('artifacts')
+        node.setAttribute('file',self.artifacts_jar)
+        root.appendChild(node)
+        
+        node = doc.createElement('organizations')
+        node.setAttribute('file',self.org_zip)
+        root.appendChild(node)
+        
+        return doc
+    
+   # def __unicode__(self):
+   #     uniArgs = (self.asl_list,self.cartago_jar,self.org_zip)
+   #     return 'Agents %s\nArtifacts %s\nOrganizations %s' % uniArgs
+    
+
+#sol = Solution()
+#x = sol.make_xml()
+#print x.toprettyxml()
+
+#print unicode(sol)
