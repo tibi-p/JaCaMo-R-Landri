45439cf524b19a998337c988fed6a420acd16e05
andreigc <andrei.geacar@gmail.com>
2012-06-21 11:49:45 +0300

    Done modifications to validator

--- solution/validate.py
+++ solution/validate.py
@@ -1,14 +1,16 @@
 from zipfile import ZipFile
+
+
 class Validator(object):
+    
+    ORG_PREFIX = 'org/aria/'
+    PROJECT_PREFIX = 'rlandri/'
+    
     def validateSolution(self,jarFile,userID):
         jar = ZipFile(jarFile, 'r')
-        path = 'org/aria/rlandri/user'+str(userID)
+        path = self.ORG_PREFIX+self.PROJECT_PREFIX+'user'+str(userID)
         for name in jar.namelist():
             if cmp(name,'META-INF/MANIFEST.MF') != 0:
                 if not name.startswith(path):
                     return False
         return True
-               # if(!name.startswith('org/aria/rlandri/user'))
-
-val = Validator()
-print val.validateSolution("dummy.jar",10)
