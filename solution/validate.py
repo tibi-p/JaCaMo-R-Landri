3c89c33cb6b98f809cdffe742d01a150f09d83a2
andreigc <andrei.geacar@gmail.com>
2012-06-21 14:08:21 +0300

    Extended specification.py. Modifications to the .mas2j generation. Addresses #4

--- solution/validate.py
+++ solution/validate.py
@@ -7,10 +7,10 @@ class Validator(object):
     PROJECT_PREFIX = 'rlandri/'
     
     def validateSolution(self,jarFile,userID):
-        jar = ZipFile(jarFile, 'r')
-        path = self.ORG_PREFIX+self.PROJECT_PREFIX+'user'+str(userID)
-        for name in jar.namelist():
-            if cmp(name,'META-INF/MANIFEST.MF') != 0:
-                if not name.startswith(path):
-                    return False
+        with ZipFile(jarFile, 'r') as jar:
+            path = self.ORG_PREFIX+self.PROJECT_PREFIX+'user'+str(userID)
+            for name in jar.namelist():
+                if cmp(name,'META-INF/MANIFEST.MF') != 0:
+                    if not name.startswith(path):
+                        return False
         return True
