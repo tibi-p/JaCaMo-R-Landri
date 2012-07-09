2d8055b2f0d103fb62bb43a091a8466cb0a70bfe
Mihai Poenaru <mihaipoenaru@gmail.com>
2012-07-09 13:11:44 +0300

    Fixed real time single player coordinator to correctly make the workspaces for clones for issue #39

--- side-projects/generic-artifacts/src/org/aria/rlandri/generic/artifacts/RealTimeSinglePlayerCoordinator.java
+++ side-projects/generic-artifacts/src/org/aria/rlandri/generic/artifacts/RealTimeSinglePlayerCoordinator.java
@@ -1,30 +1,55 @@
 package org.aria.rlandri.generic.artifacts;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
-import org.aria.rlandri.generic.artifacts.annotation.PRIME_AGENT_OPERATION;
-
-import cartago.CartagoException;
 import cartago.OPERATION;
 import cartago.OpFeedbackParam;
 
 abstract public class RealTimeSinglePlayerCoordinator extends Coordinator {
 
 	private int index = 0;
+	private String last;
 
 	@OPERATION
 	void registerAgent(OpFeedbackParam<String> privateSubenv) throws Exception{
 		super.registerAgent(null);
-	    privateSubenv.set(getOpUserName() + "_private_workspace");
+		last = "";
+		String agentName = getOpUserName();
+		if(agentName.matches(".+_[0-9]")){
+			privateSubenv.set(agentName.substring(0, agentName.lastIndexOf('_') + 1) + "private_workspace");
+		}
+		else privateSubenv.set(getOpUserName() + "_private_workspace");
 	}
 
 	// TODO @PRIME_AGENT_OPERATION
 	@OPERATION
 	private void getNextAgent(OpFeedbackParam<String> wspName) {
-		if (index == agents.keySet().size())
+		ArrayList<String> agents = new ArrayList<String>(super.agents.keySet());
+		Collections.sort(agents);
+		if (index == agents.size())
 			wspName.set("no_more");
-		else{
-			wspName.set(new ArrayList<String>(agents.keySet()).get(index) + "_private_workspace");
+		else {
+			String root;
+			if(agents.get(index).matches(".+_[0-9]")) root = agents.get(index).substring(0, agents.get(index).lastIndexOf('_'));
+			else{
+				wspName.set(agents.get(index) + "_private_workspace");
+				index++;
+				return;
+			}
+			//System.out.println(root);
+			while(root.equals(last)) {
+				//System.out.println(root);
+				index++;
+				if(index == agents.size()) {
+					wspName.set("no_more");
+					return;
+				}
+				if(agents.get(index).matches(".+_[0-9]"))root = agents.get(index).substring(0, agents.get(index).lastIndexOf('_'));
+				else root = agents.get(index);
+			}
+			last = root;
+			wspName.set(root + "private_workspace");
 			index++;
 		}
 	}
@@ -48,6 +73,13 @@ abstract public class RealTimeSinglePlayerCoordinator extends Coordinator {
 	void startSubenv() {
 		signal("startSubenv");
 		state = EnvStatus.RUNNING;
+	}
+
+	@Override
+	void finishSubenv() {
+		updateRank();
+		updateCurrency();
+		saveState();
 	}
 
 }
