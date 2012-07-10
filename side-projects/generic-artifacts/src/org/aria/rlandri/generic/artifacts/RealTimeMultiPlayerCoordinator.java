9f1b8b1a1fbb48dcf13b454c97c75f25cbe50285
Mihai Poenaru <mihaipoenaru@gmail.com>
2012-07-10 11:40:42 +0300

    Added @operation annotation to a method that was missing it.

--- side-projects/generic-artifacts/src/org/aria/rlandri/generic/artifacts/RealTimeMultiPlayerCoordinator.java
+++ side-projects/generic-artifacts/src/org/aria/rlandri/generic/artifacts/RealTimeMultiPlayerCoordinator.java
@@ -1,10 +1,12 @@
 package org.aria.rlandri.generic.artifacts;
 
+import cartago.OPERATION;
 import cartago.OpFeedbackParam;
 
 abstract public class RealTimeMultiPlayerCoordinator extends Coordinator {
 
 	@Override
+	@OPERATION
 	void registerAgent(OpFeedbackParam<String> wsp) throws Exception {
 		super.registerAgent(null);
 		wsp.set("NA");
