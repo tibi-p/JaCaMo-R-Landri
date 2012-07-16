b87b7fbd5c7ae0da468f7129d3386930dcf7aa27
andreigc <andrei.geacar@gmail.com>
2012-07-16 13:01:34 +0300

    Started work on TicTacToe

---
+++ side-projects/subenvironments/TicTacToe/src/TicTacToe.java
@@ -0,0 +1,18 @@
+import cartago.Artifact;
+import cartago.OPERATION;
+import cartago.ObsProperty;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.aria.rlandri.generic.artifacts.*;
+import org.aria.rlandri.generic.artifacts.annotation.*;
+
+/**
+ *      Artifact that implements the auction. 
+ */
+public class Roulette extends PlayerAlternatedCoordinator {
+
+}
+
