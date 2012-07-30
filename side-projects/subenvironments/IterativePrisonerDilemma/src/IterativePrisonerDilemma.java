eda4bbe9e6078a49817a95831e618a2b6f38c2f6
Andrei Geacar <andrei.geacar@gmail.com>
2012-07-30 16:47:06 +0300

    Initial commit for Iterative Prisoner's Dilemma

---
+++ side-projects/subenvironments/IterativePrisonerDilemma/src/IterativePrisonerDilemma.java
@@ -0,0 +1,23 @@
+import org.aria.rlandri.generic.artifacts.SimultaneouslyExecutedCoordinator;
+
+public class IterativePrisonerDilemma extends SimultaneouslyExecutedCoordinator {
+
+	@Override
+	protected void saveState() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void updateCurrency() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void updateRank() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
